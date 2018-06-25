7c4ac612835fbff230ee71ba2f8a4c75307eae06
Gaurav-Lamba <gauravlamba1993@gmail.com>
2018-06-25 13:30:35 +0000

    photos updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,6 +128,19 @@ $('.carousel').carousel({
         }
  }); 
  
+ $(function() {
+    var header = $(".navbar");
+  
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+  
+});
  
 
 }); // END function
